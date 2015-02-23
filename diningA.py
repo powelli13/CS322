d69089a31f26d369b5a98e475e6a23bb33d78f6b
Powell Isaac Charles <powelli@quest-OptiPlex-9020>
2015-02-23 13:37:45 -0800

    course grained is working. some write up done.

--- diningA.py
+++ diningA.py
@@ -1,4 +1,5 @@
 import threading
+import time, random
 
 # the baton, a mutex to make changing states atomic
 # TODO maybe need to make these global
@@ -7,6 +8,8 @@ n = 5 # number of philosophers
 eating = [False for i in range(n)]
 waiting = [False for i in range(n)]
 philSem = [threading.Semaphore(0) for i in range(n)]
+Philosophers = []
+
 
 class Philosopher(threading.Thread):
     def __init__(self, i):
@@ -15,10 +18,11 @@ class Philosopher(threading.Thread):
         
     def get_forks(self):
         baton.acquire()
-        waiting[self.index] = True
+        waiting[self.i] = True
         check(self.i)
         baton.release()
         philSem[self.i].acquire()
+        print "Philosopher "+str(self.i)+" is getting forks."
 
     def put_forks(self):
         baton.acquire()
@@ -26,7 +30,30 @@ class Philosopher(threading.Thread):
         eating[self.i] = False
         check((self.i+1)%n)
         check((self.i-1)%n)
+        print "Philosopher "+str(self.i)+" is releasing forks."
         baton.release()
+        
+    def eat(self):
+        print "Philosopher "+str(self.i)+" is eating."
+        time.sleep(random.randint(3,7))
+        
+    def think(self):
+        time.sleep(random.randint(3,8))
+            
+    # main execution loop
+    def run(self):
+        t = 5
+        i = 0
+        while i < t:
+            self.think()
+            self.get_forks()
+            self.eat()
+            # assert global invariant after eating
+            # before releasing forks ensure that no one next to you is eating
+            assert eating[(self.i+1)%n] == False, "Fatal error in fork use."
+            self.put_forks()
+            i = i + 1
+        print "Philosopher "+str(self.i)+" is finished."
         
 def check(i):
     if waiting[i] and not eating[(i+1)%n] and not eating[(i-1)%n]:
@@ -34,8 +61,16 @@ def check(i):
         philSem[i].release()
 
 def main():
-    print "main"
+    # create threads
+    for i in range(0,n):
+        Philosophers.append(Philosopher(i))
+        
+    # begin threads
+    for i in range(0,n):
+        Philosophers[i].start()
+        
+    print "Main is done."
     
 if __name__ == "__main__":
     main()
-    
+        
